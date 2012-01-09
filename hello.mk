271e43a56230b59e2e4e15116bd287e5769ae956
William Magato <william.magato@gmail.com>
2012-01-08 22:34:45 -0500

    renamed entry.s

--- hello.mk
+++ hello.mk
@@ -18,7 +18,7 @@ OBJECTS = $(SOURCES:%.cpp=$(OBJDIR)/%.o)
 DEPENDS = $(OBJECTS:%.o=%.d)
 
 # the entry object must be the first object listed here or the guest will crash!
-$(BINDIR)/hello-$(VMM): $(OBJDIR)/llamaos/$(VMM)/Entry.o $(OBJECTS) $(LIBDIR)/$(VMM).a $(LIBDIR)/llamaos.a $(SYSLIB_LINKS)
+$(BINDIR)/hello-$(VMM): $(OBJDIR)/llamaos/$(VMM)/x86_64.o $(OBJECTS) $(LIBDIR)/$(VMM).a $(LIBDIR)/llamaos.a $(SYSLIB_LINKS)
 	@[ -d $(@D) ] || (mkdir -p $(@D))
 	@echo linking: $@
 	@$(LD) $(LDFLAGS) -T x86_64.lds -o $@ $^
