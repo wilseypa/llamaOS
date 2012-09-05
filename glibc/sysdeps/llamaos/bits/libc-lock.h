d5f2d13dfa0fb1e31a7d8d7e9c72b2e878c25319
William Magato <william.magato@gmail.com>
2012-09-04 20:33:47 -0400

    removed old versions of 3rd party library

--- glibc/sysdeps/llamaos/bits/libc-lock.h
+++
@@ -1,9 +0,0 @@
-
-#ifndef llamaos_bits_libc_lock_h
-#define llamaos_bits_libc_lock_h
-
-#include_next <bits/libc-lock.h>
-
-#include <malloc.h>
-
-#endif  // llamaos_bits_libc_lock_h
--- glibc/sysdeps/llamaos/init-first.c
+++
@@ -1,9 +0,0 @@
-
-/* Set nonzero if we have to be prepared for more then one libc being
-   used in the process.  Safe assumption if initializer never runs.  */
-int __libc_multiple_libcs attribute_hidden = 1;
-
-/* Remember the command line argument and enviroment contents for
-   later calls of initializers for dynamic libraries.  */
-int __libc_argc attribute_hidden;
-char **__libc_argv attribute_hidden;
